8390d1de93127ca0d115340ca1af470b71c7597b
vudtipat <vudtipat@gmail.com>
2022-05-08 15:37:09 +0700

    08/05/2564

--- src/Component/ContentPage/About.js
+++ src/Component/ContentPage/About.js
@@ -5,7 +5,7 @@ const About = function (){
     return(
         <div id="about" class="container" style={{backgroundColor:'#f9f9f9'}}>
             <MyTitle Title="ABOUT ME"/>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
+            <div class='con' style={{marginTop:'50px'}}></div>
         </div>
     );
 }
